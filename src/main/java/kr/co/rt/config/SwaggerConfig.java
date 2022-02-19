d8552069fd24635d0b0bb1e53e21faf6ef0614ee
jingug1004 <jingug1004@gmail.com>
2022-02-19 22:21:42 +0900

    스웨거(Swagger API) 붙이기 성공하였다. 다음엔 NEXT.js로 프론트엔드 만들자. REST로 Swagger랑 잘 통신되기를... ㅠ_ㅠ

---
+++ src/main/java/kr/co/rt/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package kr.co.rt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Created by A on 2022-02-19 오후 9:21
+ * rt / kr.co.rt
+ * No pain, No gain!
+ * What :
+ * Why :
+ * How :
+ * << 개정이력(Modification Information) >>
+ * 수정일         수정자          수정내용
+ * -------       --------       ---------------------------
+ * 2018/04/01     김진국          최초 생성
+ * 2017/05/27     이몽룡          인증이 필요없는 URL을 패스하는 로직 추가
+ *
+ * @author 개발팀 김진국
+ * @version 1.0
+ * @see
+ * @since 2018/04/01
+ */
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kr.co.rt"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("NEXT.js + Spring Boot(Gradle, Spring Data JPA, Swagger API)")
+                .version("1.0.0")
+                .description("취미 생활 커뮤니티를 만들기 위한 백엔드 Swagger API 입니다.")
+                .build();
+    }
+}
